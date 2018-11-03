e734166d1edb367511ffd6b71d6d80887919d547
Bruce Weirdan <weirdan@gmail.com>
2018-11-03 14:43:12 +0200

    Added docs for $config parameter

--- Plugin.php
+++ Plugin.php
@@ -1,11 +1,12 @@
 <?php
 namespace Weirdan\PsalmPluginSkeleton;
 use Psalm\PluginApi;
+use SimpleXmlElement;
 
 class Plugin implements PluginApi\PluginEntryPointInterface
 {
     /** @return void */
-    public function __invoke(PluginApi\RegistrationInterface $psalm)
+    public function __invoke(PluginApi\RegistrationInterface $psalm, ?SimpleXmlElement $config = null)
     {
         // This is plugin entry point. You can initialize things you need here,
         // and hook them into psalm using RegistrationInterface
@@ -15,5 +16,11 @@ class Plugin implements PluginApi\PluginEntryPointInterface
         // ```php
         // $psalm->addStubFile(__DIR__ . '/stubs/YourStub.php');
         // ```
+
+        // Psalm allows arbitrary content to be stored under you plugin entry in
+        // its config file, psalm.xml, so you plugin users can put some configiration
+        // values there. They will be provided to your plugin entry point in $config
+        // parameter, as a SimpleXmlElement object. If there's no configuration present,
+        // null will be passed instead.
     }
 }
